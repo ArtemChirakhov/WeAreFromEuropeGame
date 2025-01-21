305f2523a856287a8240c679c21bf5e6b2fdd59d
ArtemChirakhov <achiraxov@yandex.ru>
2025-01-22 02:52:41 +0300

    Вращение оружия в руках с направлением к курсору.

---
+++ Assets/SwordScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+
+public class SwordScript : MonoBehaviour
+{
+    private Camera _camera;
+    [SerializeField] private GameObject Player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Player.transform.position;
+        if(diference.x <= 0){
+            float arctg = Mathf.Atan2(diference.y, diference.x);
+            float gradient = diference.y / diference.x;
+            float arcctg = 1 / arctg;
+            float x = arctg * 180 / Mathf.PI;
+            transform.rotation = Quaternion.Euler(0,0,x - 90);
+            transform.position = Player.transform.position + new Vector3(Mathf.Cos(arctg), Mathf.Sin(arctg));
+        }
+        else{
+            float arctg = Mathf.Atan2(diference.y, diference.x);
+            float gradient = diference.y / diference.x;
+            float arcctg = 1 / arctg;
+            float x = arctg * 180 / Mathf.PI;
+            transform.rotation = Quaternion.Euler(0,0,x - 90);
+            transform.position = Player.transform.position + new Vector3(Mathf.Cos(arctg), Mathf.Sin(arctg));
+        }
+    }
+}
