8d30e662d33e3bae046868d0377eb33c6be3e39d
dbesman <besman.dmitry@gmail.com>
2025-03-11 23:31:57 +0300

    configured mvp layout for title menu, for it imported unity TMP package

--- Assets/Scripts/UI/SettingButtonTitle.cs
+++ Assets/Scripts/UI/SettingButtonTitle.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TitleMenu : MonoBehaviour
+public class SettingsButtonTitle : MonoBehaviour
 {
 
     private static readonly Dictionary<String, String> keybindsDefault = new()
@@ -15,6 +15,19 @@ public class TitleMenu : MonoBehaviour
     };
 
     public Dictionary<String, String> keybindsAction = new(keybindsDefault);
+    private Dictionary<String, String> GetKeyBinds()
+    {
+        Dictionary<String, String> keybindsDefault = new()
+        {
+            ["Up"] = "W",
+            ["Left"] = "A",
+            ["Down"] = "S",
+            ["Right"] = "D",
+            ["Pause"] = "Esc"
+        };
+
+        return keybindsDefault;
+    }
     
     void Start()
     {
