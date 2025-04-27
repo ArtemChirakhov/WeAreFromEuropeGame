07c2fc976575def48f2c37e9117b0c6d1242ff3d
dbesman <besman.dmitry@gmail.com>
2025-04-27 21:42:47 +0300

    removed old uneccesary boilerplate funcs

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -7,6 +7,17 @@ public class GameHandler : MonoBehaviour
     public static GameHandler _instance { get; private set; }
     public static MainActionMap playerActions;
 
+    private static Dictionary<String, String> defaultKeybinds = new()
+    {
+        ["Up"] = "W",
+        ["Left"] = "A",
+        ["Down"] = "S",
+        ["Right"] = "D",
+        ["Pause"] = "Esc"
+    };
+
+    public static Dictionary<String, String> currentKeybinds = new(defaultKeybinds);
+    
     void Awake()
     {
         playerActions = new();
